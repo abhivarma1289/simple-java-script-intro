590b6a152e2a4e1da5d3a624c585391950b0b920
abhivarma <abhilash.dongari2@gmail.com>
2023-02-05 12:44:36 +0530

    modeule callbacks and error handlings

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const rect=require('./rectangle');
 
 function solverec(l,b){
     console,console.log("length = "+l+"  breadt = "+b);
-    if(l<=0 || b<=0) {
-        console.log("dimesions are invalid ,l and b must should be grater than 0(zero)");
-    }
-    else{
-       console.log("area of rectanle is :"+ rect.area(l,b));
-       console.log("perimeter of rectanle is :"+ rect.perimeter(l,b));
-    }
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log("ERROR :" ,err.message);
+        }
+        else{ 
+            console.log("Area of rectangle of length l "+l +"  and breadth "+ b +"is :" + rectangle.area());
+            console.log("perimeter of rectangle of length l " +l +"  and breadth "+ b +"is :" + rectangle.perimeter());
+        }
+    });
+    console.log("This statment is after funcion call rect()");
+   
 }
 solverec(2,3);
-solverec(0,3);
-solverec(-1,3);
-solverec(2,4);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,18 @@
-exports.area =(x,y)=>(2*(x+y));
-exports.perimeter =(x,y)=>(x*y);
+module.exports=(x,y,callback)=>{
+    if(x<=0 || y<=0) {
+        setTimeout(()=>
+            callback(new Error("dimesions are invalid ,l and b must should be grater than 0(zero")
+            ,null),
+            2000);
+    }
+    else{
+        setTimeout(()=>
+            callback(null,
+            {
+                area:()=>(x*y),
+                perimeter:()=>(2*(x+y))
+
+            }),
+            2000);
+    }
+}
